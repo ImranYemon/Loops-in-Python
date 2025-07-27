e4df91023f861b5b36ab3fe2ef03c19b869bc7f5
Imran Yemon <imranyemons24@gmail.com>
2025-07-27 23:11:06 +0600

    Add files via upload

---
+++ Break_continue_pass-statement.py
@@ -0,0 +1,27 @@
+# Break statement 
+list =[2,5,6,9,4,8,3,6,7,8,10]
+
+for a in list :
+    if (a==7):
+        break
+    print(a)
+
+
+# contunue Statement 
+for a in list :
+    if (a==7):
+        continue
+    print(a)
+
+
+# Pass Statement
+a=0
+for a in range(4) :
+    pass
+
+i= 0
+while(i<10):
+    i=i+1 
+    print(i)
+   
+
